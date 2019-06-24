4a52455a35b322049ce7e065ae14a4ca7544cc7a
Morgan Aubert <morgan@aubert.email>
2019-06-23 22:58:17 -0400

    Update routes resolver helper

--- js_routes/static/js/routes/resolver.js
+++ js_routes/static/js/routes/resolver.js
@@ -10,37 +10,32 @@ window.reverseUrl = (function () {
     }
 
     const tokens = urlPattern.match(argRegex);
+    if (!tokens && arguments[1] !== undefined) {
+      throw "URL '" + name + "' does not expect any arguments.";
+    }
 
     if (typeof (arguments[1]) == 'object' && !Array.isArray(arguments[1])) {
-      if (tokens) {
-        for (var i=0; i < tokens.length; i += 1) {
-          const token = tokens[i];
-          const argName = token.slice(1, -1);
-          const argValue = arguments[1][argName];
+      for (var i=0; i < tokens.length; i += 1) {
+        const token = tokens[i];
+        const argName = token.slice(1, -1);
+        const argValue = arguments[1][argName];
 
-          if (argValue === undefined) {
-            throw "Argument '" + argName + "' not provided.";
-          }
-          url = url.replace(token, argValue);
+        if (argValue === undefined) {
+          throw "Argument '" + argName + "' not provided.";
         }
-      } else {
-        throw "URL '" + name + "' does not expect any arguments.";
+        url = url.replace(token, argValue);
       }
     } else if (arguments[1] !== undefined) {
       const argsArray = Array.isArray(arguments[1]) ? arguments[1] :
         Array.prototype.slice.apply(arguments, [1, arguments.length]);
-      if (tokens) {
-        if (tokens.length !== argsArray.length) {
-          throw "Wrong number of arguments ; espected " + tokens.length + " arguments.";
-        }
+      if (tokens.length !== argsArray.length) {
+        throw "Wrong number of arguments ; espected " + tokens.length + " arguments.";
+      }
 
-        for (var i=0; i < tokens.length; i += 1) {
-          const token = tokens[i];
-          const argValue = argsArray[i];
-          url = url.replace(token, argValue);
-        }
-      } else {
-        throw "URL '" + name + "' does not expect any arguments.";
+      for (var i=0; i < tokens.length; i += 1) {
+        const token = tokens[i];
+        const argValue = argsArray[i];
+        url = url.replace(token, argValue);
       }
     }
 
